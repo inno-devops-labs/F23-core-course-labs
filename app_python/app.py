95237d7953e073cce01183ab56d2f07fdf775fc5
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-09-01 12:25:38 +0300

    add styling, linting&formatting

--- app_python/app.py
+++ app_python/app.py
@@ -4,22 +4,26 @@ import pytz
 
 app = Flask(__name__)
 
+
 def get_moscow_time():
-    moscow_timezone = pytz.timezone('Europe/Moscow')
+    moscow_timezone = pytz.timezone("Europe/Moscow")
     moscow_time = datetime.datetime.now(moscow_timezone)
-    return moscow_time.strftime('%Y-%m-%d %H:%M:%S')
+    return moscow_time.strftime("%Y-%m-%d %H:%M:%S")
+
 
 @app.route("/")
 def show_time():
     moscow_time = get_moscow_time()
-    return render_template('index.html', moscow_time=moscow_time)
+    return render_template("index.html", moscow_time=moscow_time)
 
-@app.route('/get_time/')
+
+@app.route("/get_time/")
 def get_time():
-    moscow_timezone = pytz.timezone('Europe/Moscow')
+    moscow_timezone = pytz.timezone("Europe/Moscow")
     moscow_time = datetime.datetime.now(moscow_timezone)
-    formatted_time = moscow_time.strftime('%Y-%m-%d %H:%M:%S')
+    formatted_time = moscow_time.strftime("%Y-%m-%d %H:%M:%S")
     return formatted_time
 
-if __name__ == '__main__':
-    app.run()
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- app_python/test_app.py
+++ app_python/test_app.py
@@ -2,19 +2,21 @@ import unittest
 from app import app
 import time
 
+
 class TestApp(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
-    
+
     def test_time_updates_on_refresh(self):
-        response_1 = self.app.get('/')
+        response_1 = self.app.get("/")
         time.sleep(2)
-        response_2 = self.app.get('/')
+        response_2 = self.app.get("/")
 
         content_1 = response_1.get_data(as_text=True)
         content_2 = response_2.get_data(as_text=True)
 
         self.assertNotEqual(content_1, content_2)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
