1110d8da18ab00ccb0f5b009f532fb93fee82897
Jaffar <jacepac264@gmail.com>
2023-09-01 20:41:09 +0300

    [add] pre-commit hooks for app_python

--- app_python/app.py
+++ app_python/app.py
@@ -3,15 +3,16 @@ from datetime import datetime, timezone, timedelta
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def moscow_time():
-     # Get the current UTC time
-     utc_time = datetime.now(timezone.utc)
+    # Get the current UTC time
+    utc_time = datetime.now(timezone.utc)
 
-     # Calculate the time difference for MSK (UTC+3)
-     msk_time = utc_time + timedelta(hours=3)
+    # Calculate the time difference for MSK (UTC+3)
+    msk_time = utc_time + timedelta(hours=3)
 
-     # Format the time as a string
-     msk_time_str = msk_time.strftime('%Y-%m-%d %H:%M:%S')
+    # Format the time as a string
+    msk_time_str = msk_time.strftime("%Y-%m-%d %H:%M:%S")
 
-     return f'Current Time in MSK: {msk_time_str}'
+    return f"Current Time in MSK: {msk_time_str}"
