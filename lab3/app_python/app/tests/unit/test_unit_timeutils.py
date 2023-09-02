f1f0079e038f445dcc6e43e971ac4171f4216297
Artem Bulgakov <artyombulgakov@gmail.com>
2023-09-02 03:34:01 +0300

    add lab3

---
+++ lab3/app_python/app/tests/unit/test_unit_timeutils.py
@@ -0,0 +1,11 @@
+import pytest
+from timeutils import timeutils
+
+
+def test_timeutils_get_time():
+    from zoneinfo import ZoneInfo
+    import datetime
+
+    time = datetime.datetime.now(tz=ZoneInfo(
+        "Europe/Moscow")).strftime("%H:%M:%S")
+    assert timeutils.get_time() == "Current time in Moscow: " + time
