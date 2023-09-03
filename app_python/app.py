abd7761ec38eac81c804c9d738ad077e9facd376
Eugene K <evgn.worker@gmail.com>
2023-09-03 20:38:40 +0300

    Add PYTHON.md

--- app_python/app.py
+++ app_python/app.py
@@ -5,26 +5,21 @@ import pytz
 import logging
 from logging.handlers import RotatingFileHandler
 
-# Set up logging
+# Set up file and console logging
 logger = logging.getLogger()
 logger.setLevel(LOG_LEVEL)
 
-# Create a file handler and set its level
 file_handler = RotatingFileHandler(LOG_FILENAME, maxBytes=LOG_MAX_BYTES, backupCount=LOG_BACKUP_COUNT)
 file_handler.setLevel(LOG_LEVEL)
 
-# Create a console handler and set its level
 console_handler = logging.StreamHandler()
 console_handler.setLevel(LOG_LEVEL)
 
-# Create a formatter using the extracted LOG_FORMAT
 formatter = logging.Formatter(LOG_FORMAT)
 
-# Add formatter to both handlers
 file_handler.setFormatter(formatter)
 console_handler.setFormatter(formatter)
 
-# Add the handlers to the logger
 logger.addHandler(file_handler)
 logger.addHandler(console_handler)
 
@@ -34,10 +29,14 @@ app = Flask(__name__)
 @app.route('/')
 def moscow_time():
     try:
+        # Fetch Moscow time
         moscow_tz = pytz.timezone('Europe/Moscow')
         current_time = datetime.now(moscow_tz).strftime('%Y-%m-%d %H:%M:%S')
         app.logger.info('Moscow time fetched successfully.')
+        
+        # Render HTML
         return render_template("time.html", current_time=current_time)
+    
     except Exception as e:
         app.logger.error(f"Error fetching Moscow time: {e}")
         return "Error fetching Moscow time", 500
