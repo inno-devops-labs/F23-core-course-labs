5f11cc3aa29f401bfc391a4623eed23588cdd816
Anatoliy <shvarts.tolik@mail.ru>
2023-09-04 22:48:29 +0300

    Update PYTHON.md

--- app_python/src/__main__.py
+++ app_python/src/__main__.py
@@ -4,8 +4,24 @@ from fastapi import FastAPI
 from app_python.src.time.time_router import time_router
 from config import config
 
+description = """
+QUINER API helps you do awesome stuff. 🚀
 
-app = FastAPI()
+## Time
+
+You can get current Moscow time and data.
+"""
+
+app = FastAPI(
+    title="QuinerApp",
+    description=description,
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Anatoliy Shvarts",
+        "email": "a.shvarts@innopolis.university",
+    }
+)
 
 app.include_router(time_router)
 
