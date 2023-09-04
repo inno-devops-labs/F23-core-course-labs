c743520bd24db07a110ba396e74080fdde22b80f
aibek.bakirov <aibek.bakirov99@gmail.com>
2023-09-05 02:03:43 +0300

    commit tests/test_app.py and tests/test_refresh_time.py for testing purpose

---
+++ app_python/tests/test_app.py
@@ -0,0 +1,15 @@
+import pytest
+from app import app
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
+def test_index(client):
+    response = client.get("/")
+    assert b"Current Time in Moscow:" in response.data
+    assert b"Date:" in response.data
+    assert b"Time:" in response.data
