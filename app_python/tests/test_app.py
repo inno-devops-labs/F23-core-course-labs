c4196aabdba00c40f3eaff2c8b627218c1e5b17c
Daniil <daniil.okrug@gmail.com>
2023-09-04 12:15:52 +0300

    feat(lab1): flake8 linting

--- app_python/tests/test_app.py
+++ app_python/tests/test_app.py
@@ -2,16 +2,19 @@ import pytest
 import time
 from app import app
 
+
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     client = app.test_client()
     yield client
 
+
 def test_display_time(client):
     response = client.get('/')
     assert b'Current Time in Moscow' in response.data
     assert response.status_code == 200
+
 
 def test_time_update(client):
     response1 = client.get('/')
