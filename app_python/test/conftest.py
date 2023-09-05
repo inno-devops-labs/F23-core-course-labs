a7839b14fd91b8ae14e4888be1eee0320efbe002
Nikita Pozdniakov <nikitosing4@mail.ru>
2023-09-05 22:20:39 +0300

    Complete Lab1

---
+++ app_python/test/conftest.py
@@ -0,0 +1,29 @@
+
+
+from app import init
+import pytest
+import os
+import sys
+current_dir = os.path.dirname(os.path.abspath(__file__))
+source_dir = os.path.abspath(os.path.join(current_dir, ".."))
+sys.path.append(source_dir)
+
+
+@pytest.fixture()
+def app():
+    app = init()
+
+    app.config.update({
+        "TESTING": True,
+    })
+    yield app
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
