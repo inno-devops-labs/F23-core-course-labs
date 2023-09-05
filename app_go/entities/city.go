609c4738a07540f2e390c14b72b7999e442c78f8
rkBekzat <bekzatrakhimbayev@gmail.com>
2023-09-06 01:28:58 +0300

    lab1 done

---
+++ app_go/entities/city.go
@@ -0,0 +1,6 @@
+package entities
+
+type Weather struct {
+	Celsius string
+	Err     error
+}
--- app_go/weather.go
+++ app_go/weather.go
@@ -1,54 +1,12 @@
 package main
 
 import (
-	"html/template"
-	"log"
+	"app_go/handler"
 	"net/http"
-
-	"github.com/gocolly/colly"
 )
 
-type weather struct {
-	Celsius string
-	err     error
-}
-
-var result weather
-
-func handleError(err error) {
-	if err != nil {
-		log.Println(err.Error())
-	}
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	html := template.Must(template.ParseFiles("templates/index.html"))
-	city := r.URL.Path[1:]
-	parse(city)
-	if result.err == nil {
-		err := html.Execute(w, city+" temperature is "+result.Celsius)
-		handleError(err)
-		return
-	}
-	err := html.Execute(w, result.err.Error())
-	handleError(err)
-}
-
-func parse(city string) {
-	c := colly.NewCollector()
-	c.OnHTML("div.today-temperature span[dir=ltr]", func(element *colly.HTMLElement) {
-		result.Celsius = element.Text
-	})
-	err := c.Visit("https://www.meteoprog.com/weather/" + city + "/")
-	if err != nil {
-		result.err = err
-		return
-	}
-	result.err = nil
-}
-
 func main() {
-	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/", handler.IndexHandler)
 	err := http.ListenAndServe(":3000", nil)
-	handleError(err)
+	handler.Error(err)
 }
