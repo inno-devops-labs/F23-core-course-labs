39c2addc17338ef395b77ac0232709bdd51a271a
Khalilov Vagif Azerovich <vakhalilov@ozon.ru>
2023-09-05 18:27:33 +0300

    Add python app

---
+++ app_python/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from datetime import datetime, timezone, timedelta
+
+app = Flask(__name__)
+
+@app.route('/')
+def current_time():
+    # Get time in Moscow
+    moscow_time = datetime.now(timezone(timedelta(hours=3))) 
+    return f'Current time in Moscow: {moscow_time.strftime("%H:%M:%S")}'
+
+if __name__ == '__main__':
+    app.run()
