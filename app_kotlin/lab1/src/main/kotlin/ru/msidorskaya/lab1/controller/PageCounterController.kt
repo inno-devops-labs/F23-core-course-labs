46b6ba12d71f2ec048c790c1f761452c6e96b053
ritasidorska <m.sidorskaya@innopolis.university>
2023-09-05 21:58:43 +0300

    Implemented PageCounterController that shows how many times page was opened

---
+++ app_kotlin/lab1/src/main/kotlin/ru/msidorskaya/lab1/controller/PageCounterController.kt
@@ -0,0 +1,18 @@
+package ru.msidorskaya.lab1.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class PageCounterController {
+
+    @ResponseBody
+    @GetMapping("/")
+    fun getCurrentOpenNumber() = "Воу, Вы открывали эту страницу уже $launchNumber раз!"
+            .also { launchNumber += 1 }
+
+    companion object {
+        var launchNumber = 1
+    }
+}
