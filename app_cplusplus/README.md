715253659b293600d2e12768c55b8e7439d9ad43
elestrias <rus8-2002@mail.ru>
2023-09-06 09:13:04 +0300

    Extension of app_cplusplus

--- app_cplusplus/README.md
+++ app_cplusplus/README.md
@@ -10,4 +10,5 @@ This web app is playing in guessing game! It takes numbers you say and gives you
     1. conan install . --output-folder=cmake-build-debug --build=missing
     2. cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -G Ninja -S app_cplusplus -B buildDirectory
     3. cmake --build ./buildDirectory
-    4. Run 'main <port you want to run on>' from buildDirectory
+    4. Run 'startServer <port you want to run on>' from buildDirectory
+    5. Run 'checkServer' target to test application
---
+++ app_cplusplus/src/Server.cpp
@@ -0,0 +1,64 @@
+#include "Server.hpp"
+#include <boost/asio.hpp>
+#include <fstream>
+
+int guessingNumber = 234;
+
+bool is_number(const std::string &s) {
+    return !s.empty() && std::find_if(s.begin(),
+                                      s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
+}
+
+[[noreturn]] void start_server(int port, int cycle) {
+    boost::asio::io_service io_service;
+    boost::asio::ip::tcp::acceptor acceptor(io_service,
+                                            boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port));
+
+    for(int i = 0; i < cycle;) {
+        if (cycle != -1) {
+            ++i;
+        }
+        boost::asio::ip::tcp::socket socket(io_service);
+        acceptor.accept(socket);
+
+        boost::asio::streambuf buffer;
+        boost::asio::read_until(socket, buffer, "\r\n\r\n");
+
+        std::string method, path, http_version;
+        std::istream request_stream(&buffer);
+        request_stream >> method >> path >> http_version;
+
+        std::string response = "HTTP/1.1 200 OK\r\n"
+                               "Content-Type: text/html\r\n\r\n";
+
+
+        std::string htmlResponse;
+        std::ifstream in("index.html");
+        std::string str((std::istreambuf_iterator<char>(in)),
+                        std::istreambuf_iterator<char>());
+        response += str;
+
+        std::string newMessage;
+        std::string messageStr = "message=";
+        size_t messageIt = path.find(messageStr);
+        if (messageIt != std::string::npos) {
+            auto strNumber = path.substr(messageIt + messageStr.size(), std::string::npos);
+            if (is_number(strNumber)) {
+                int num = std::stoi(strNumber);
+                if (num == guessingNumber) {
+                    newMessage = "You win!";
+                } else if (num < guessingNumber) {
+                    newMessage = "You write smaller number then guessing number";
+                } else {
+                    newMessage = "You write bigger number then guessing number";
+                }
+
+                std::string paste = "<h1>Number 1 to 1000</h1>";
+                size_t toPaste = response.find(paste);
+                response.insert(toPaste + paste.size(), "<h2>" + newMessage + "</h2>");
+            }
+        }
+
+        boost::asio::write(socket, boost::asio::buffer(response));
+    }
+}
