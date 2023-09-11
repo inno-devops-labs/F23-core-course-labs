7fd890ab689ab7e10edc8c355db335bccd10ae83
Arseniy172 <LEvochkin-2002@mail.ru>
2023-09-11 12:04:49 +0300

    Lab2

---
+++ app_python/MoscowTime.py
@@ -0,0 +1,44 @@
+import requests
+import json
+import calendar
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def MoscowTime():
+    response = requests.get('https://api.api-ninjas.com/v1/worldtime?city={}'.format("Moscow"), timeout=5,
+                            headers={'X-Api-Key': 'oZDQTEM0MJpKNshpblkFBQ==RRsPxd52ToPtLq75'})
+
+    if response.status_code == requests.codes.ok:
+        res = json.loads(response.text)
+        time = res['hour']+":"+res['minute']+":"+res['second']+", "+res['day']+" "+\
+               str(calendar.month_name[int(res['month'])])+" "+\
+               res['year']+", "+res['day_of_week']
+        return """
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <title>Moscow Time App</title>
+            </head>
+            <body>
+               <h1>Current Moscow Time is</h1>
+               <h2> """+time+""" </h2>
+            </body>
+            </html>
+        """
+    return """
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <title>Moscow Time App</title>
+                </head>
+                <body>
+                   <h1>Error occurred while loading a Moscow Time</h1>
+                </body>
+                </html>
+            """
+
+app.run(debug=True)
---
+++ app_python/README.md
@@ -0,0 +1,19 @@
+# Moscow Time App
+
+## Overview
+
+Python Web App built using Flask Framework and ApiNinjas for requesting Moscow Time
+
+## Usage
+
+1. Install Python 3.9
+2. Install pip
+3. Install Flask
+4. Enter app_python repository
+5. Run `flask run`
+6. An application will open at http://127.0.0.1:5000/
+
+
+
+#### Contact: Arseniy Levochkin, B20-SD-02, a.levochkin@innopolis.university
+
