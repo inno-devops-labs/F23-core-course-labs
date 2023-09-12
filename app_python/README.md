1786088edd3c22f04e53a589cc95002d764fe955
tatiana <monday240220@gmail.com>
2023-09-12 20:10:16 +0300

    lab2

--- app_python/README.md
+++ app_python/README.md
@@ -7,3 +7,10 @@ The application contains ```app/__init__.py``` file, which contains the root end
 You can run the application by running the ```run.py``` file.
 
 ![screenshot of working application](img.png)
+
+# Docker
+
+How to launch:
+1. Build with ```sudo docker image build -t my_app .```
+2. Pull with ```docker image pull tnechepurencko/my_app```
+3. Run with ```sudo docker run -d -p 5000:5000 my_app```
--- app_python/run.py
+++ app_python/run.py
@@ -1,3 +1,21 @@
-from app_python.app import app
+from datetime import datetime
+import pytz
+from flask import Flask
 
-app.run(debug=True)
+
+TIMEZONE = 'Europe/Moscow'
+DATETIME_FORMAT = "%d/%m/%Y %H:%M:%S"
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_moscow_time():
+    moscow_time = datetime.now(pytz.timezone(TIMEZONE))
+    formatted_time = moscow_time.strftime(DATETIME_FORMAT)
+    return formatted_time
+
+
+
+app.run(debug=True, host='0.0.0.0')
