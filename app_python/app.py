faadf54b4cca191f782638b2a0ee933fedd74f6d
MaximInnopolis <71584246+MaximInnopolis@users.noreply.github.com>
2023-09-13 02:26:13 +0300

    Task 1 of lab2 completed

--- app_python/app.py
+++ app_python/app.py
@@ -1,7 +1,11 @@
+import argparse
+from waitress import serve
 from flask import Flask, render_template
 from datetime import datetime, timezone, timedelta
 
 app = Flask(__name__)
+parser = argparse.ArgumentParser()
+parser.add_argument("-prod", "--production", action='store_true')
 
 MOSCOW_TZ = timezone(timedelta(hours=3))  # Moscow time zone (UTC+3)
 
@@ -13,4 +17,9 @@ def display_time():
 
 
 if __name__ == '__main__':
-    app.run()
+    is_prod = parser.parse_args().production
+
+    if is_prod:
+        serve(app, port=80)
+    else:
+        app.run()
