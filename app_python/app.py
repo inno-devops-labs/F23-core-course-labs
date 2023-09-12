38178241015e3f529ab6ac5b20f8bc55ec9b6a53
DyllasDek <d.korneenko@innopolis.university>
2023-09-12 18:54:45 +0300

    Adapt solution for docker

--- app_python/app.py
+++ app_python/app.py
@@ -4,17 +4,19 @@ import pytz
 
 app = Flask(__name__)
 
+
 @app.route('/')
-#Get current time in Moscow
+# Get current time in Moscow
 def current_time_moscow():
-    #Set timezone
+    # Set timezone
     moscow_tz = pytz.timezone('Europe/Moscow')
-    #Get current time in Moscow
+    # Get current time in Moscow
     moscow_time = datetime.now(moscow_tz)
-    #Format time
+    # Format time
     formatted_time = moscow_time.strftime('%Y-%m-%d %H:%M:%S')
-    #Return time
+    # Return time
     return f'Current time in Moscow: {formatted_time}'
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
