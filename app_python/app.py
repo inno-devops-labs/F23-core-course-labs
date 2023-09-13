cb57d7d9fd9860799f093b662a24ef6c5cff92b6
m4k4rich <m4k4rich@gmail.com>
2023-09-13 03:03:41 +0300

    fix: update port and host

--- app_python/app.py
+++ app_python/app.py
@@ -12,4 +12,4 @@ def display_time():
     return render_template('time.html', time=formatted_time)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
