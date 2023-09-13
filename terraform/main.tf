e40c71c74f76e149fd1ab0b6fc416004aeb929cf
lcensies <0xf1o2732@proton.me>
2023-09-13 11:27:28 +0300

    Added tw terraform

---
+++ terraform/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_version = ">= 1.2.0"
+  required_providers {
+    twc = {
+      source = "tf.timeweb.cloud/timeweb-cloud/timeweb-cloud"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "twc" {
+  token = var.timeweb_token
+}
+
+
+data "twc_configurator" "configurator" {
+  location = "nl-1"
+  disk_type = "nvme"
+}
+
+# Create new SSH key
+# SSH key can be imported by specifying the numeric identifier from URL
+# terraform import twc_ssh_key.example 42
+resource "twc_ssh_key" "timeweb-0xf" {
+  name = "timeweb-0xf"
+  body = file(var.timeweb_ssh_pubkey)
+}
+
+
+data "twc_os" "os" {
+  name = "ubuntu"
+  version = "22.04"
+}
+
+
+# Server can be imported by specifying the numeric identifier (from URL)
+# terraform import twc_server.example 42 
+resource "twc_server" "cygnus" {
+  name = var.instance_name
+  os_id = data.twc_os.os.id
+
+  configuration {
+    configurator_id = data.twc_configurator.configurator.id
+    disk = 1024 * 50
+    cpu = 2
+    ram = 1024 * 4
+  }
+
+  ssh_keys_ids = [twc_ssh_key.timeweb-0xf.id]
+
+
+}
---
+++ terraform/outputs.tf
@@ -0,0 +1,20 @@
+
+locals {
+  timeweb_servers = [
+	  {
+		name = twc_server.cygnus.name
+		disk = format("%d GB", twc_server.cygnus.configuration[0].disk)
+		cpu  = twc_server.cygnus.cpu
+		ram  = format("%d GB", twc_server.cygnus.configuration[0].ram)
+	  }
+ ]
+  timeweb_ssh_keys = [twc_ssh_key.timeweb-0xf.name]
+}
+
+output "servers" {
+  value = local.timeweb_servers
+}
+
+output "ssh_keys" {
+  value = local.timeweb_ssh_keys
+}
