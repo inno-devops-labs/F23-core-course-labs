b1e612300464654f46e61c01386f8dc430f37bd9
Daniil <daniil.okrug@gmail.com>
2023-09-18 00:16:46 +0300

    feat(lab3): updated tests for python app

--- app_python/tests/test_app.py
+++ app_python/tests/test_app.py
@@ -1,3 +1,4 @@
+import re
 import pytest
 import time
 from app import app
@@ -10,11 +11,16 @@ def client():
     yield client
 
 
-def test_display_time(client):
+def test_index_route(client):
     response = client.get('/')
     assert b'Current Time in Moscow' in response.data
     assert response.status_code == 200
 
+def test_get_time_route(client):
+    response = client.get('/time')
+    assert response.status_code == 200
+
+    assert bool(re.match(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', response.get_data(as_text=True)))
 
 def test_time_update(client):
     response1 = client.get('/')
@@ -25,3 +31,11 @@ def test_time_update(client):
     print(response2)
 
     assert response1.data != response2.data
+
+def test_index_template(client):
+    response = client.get('/')
+    assert b"Current Time in Moscow:" in response.data
+    assert response.status_code == 200
+
+    assert b"<title>Moscow Time</title>" in response.data
+    assert b'<h1>Current Time in Moscow:</h1>' in response.data
