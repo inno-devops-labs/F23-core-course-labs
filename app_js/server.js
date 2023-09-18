4ff6b7c95f3a65e1f076f67dcdf719e8a92bb516
purfreak <tyrefrtter@gmail.com>
2023-09-18 05:12:51 +0300

    added JS part

--- app_js/server.js
+++ app_js/server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`Web application is running at http://localhost:${port}`);
+  console.log(`Web application is running at http://localhost:${port}`);
 });
--- app_js/test/test.js
+++ app_js/test/test.js
@@ -5,27 +5,27 @@ const app = require('../app');
 const expect = chai.expect;
 
 describe('GET /', function() {
-    this.timeout(5000);
+  this.timeout(5000);
 
-    it('should return different times in Moscow with a delay between requests', async function() {
-        const response1 = await request(app).get('/');
-        expect(response1.statusCode).to.equal(200);
-        expect(response1.text).to.include('Current time in Moscow');
-        const time1 = response1.text;
+  it('should return varying Moscow times with request delay', async function() {
+    const response1 = await request(app).get('/');
+    expect(response1.statusCode).to.equal(200);
+    expect(response1.text).to.include('Current time in Moscow');
+    const time1 = response1.text;
 
-        return new Promise((resolve) => {
-            // Delay of 2 seconds
-            setTimeout(async () => {
-                // Second request after delay
-                const response2 = await request(app).get('/');
-                expect(response2.statusCode).to.equal(200);
-                expect(response2.text).to.include('Current time in Moscow');
-                const time2 = response2.text;
+    return new Promise((resolve) => {
+      // Delay of 2 seconds
+      setTimeout(async () => {
+        // Second request after delay
+        const response2 = await request(app).get('/');
+        expect(response2.statusCode).to.equal(200);
+        expect(response2.text).to.include('Current time in Moscow');
+        const time2 = response2.text;
 
-                expect(time1).to.not.equal(time2);
+        expect(time1).to.not.equal(time2);
 
-                resolve();
-            }, 2000);
-        });
+        resolve();
+      }, 2000);
     });
+  });
 });
