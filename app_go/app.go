51903fafbfd5cb2a13a9b915d09b0fe1c73a0346
MaximInnopolis <71584246+MaximInnopolis@users.noreply.github.com>
2023-09-20 02:45:37 +0300

    Update

--- app_go/app.go
+++ app_go/app.go
@@ -8,14 +8,14 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Got current time в Moscow
+		// Got current time in Moscow
 		loc, _ := time.LoadLocation("Europe/Moscow")
 		currentTime := time.Now().In(loc).Format("2006-01-02 15:04:05")
 
-		// Send time в response
+		// Send time in response
 		fmt.Fprintf(w, "Current Moscow Time: %s", currentTime)
 	})
 
-	// Слушаем порт 8080
+	// Listen port 8080
 	http.ListenAndServe(":8080", nil)
 }
