ebdefdc7717b83924c6ba643bfe310aef0f4b7e5
MaximInnopolis <71584246+MaximInnopolis@users.noreply.github.com>
2023-09-20 00:50:11 +0300

    Added comments

--- app_python/app.py
+++ app_python/app.py
@@ -1,4 +1,9 @@
-# Import necessary modules
+"""
+This module provides a simple web application that displays the current time in Moscow.
+
+It utilizes the Flask framework for web development and serves the Moscow time on an HTML page.
+"""
+
 import argparse
 from datetime import datetime, timezone, timedelta
 from waitress import serve
@@ -14,13 +19,17 @@ parser.add_argument("-prod", "--production", action='store_true')
 # Define the Moscow time zone (UTC+3) for use in datetime calculations
 MOSCOW_TZ = timezone(timedelta(hours=3))  # Moscow time zone (UTC+3)
 
-
 # Define a route that displays the current Moscow time
 @app.route('/')
 def display_time():
+    """
+    Display the current time in Moscow.
+
+    Returns:
+        str: A string representing the current time in the format 'YYYY-MM-DD HH:MM:SS'.
+    """
     moscow_time = datetime.now(MOSCOW_TZ).strftime('%Y-%m-%d %H:%M:%S')
     return render_template('time.html', moscow_time=moscow_time)
-
 
 # Entry point for the script
 if __name__ == '__main__':
