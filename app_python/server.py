5f2ce2aff3f071627ed9c549d2d5c5982a479da5
Сафина Алина Азатовна <safina.aa@gazprom-neft.ru>
2023-09-19 11:37:20 +0300

    small fix

--- app_python/server.py
+++ app_python/server.py
@@ -3,7 +3,6 @@ from zoneinfo import ZoneInfo
 
 from fastapi import FastAPI
 from starlette.requests import Request
-from starlette.templating import Jinja2Templates
 from app_python.config import config
 
 app = FastAPI()
@@ -14,13 +13,12 @@ FORMAT = '%H:%M:%S'
 @app.get("/")
 async def root(request: Request):
     return config.templates.TemplateResponse(name="page.html",
-                                                 context={"request": request,
-                                                          "Title": "DevOps lab",
-                                                          "Msg": "Hello! This is DevOps course lab by Safina Alina",
-                                                          # noqa: E501
-                                                          "Href": "/time",
-                                                          "LinkMsg": "Current time in Moscow"},  # noqa: E501
-                                                 status_code=200)
+                                             context={"request": request,
+                                                      "Title": "DevOps lab",
+                                                      "Msg": "Hello! This is DevOps course lab by Safina Alina",  # noqa: E501
+                                                      "Href": "/time",
+                                                      "LinkMsg": "Current time in Moscow"},  # noqa: E501
+                                             status_code=200)
 
 
 @app.get("/time")
@@ -29,8 +27,7 @@ async def time(request: Request):
                                              context={"request": request,
                                                       "Title": "DevOps lab",
                                                       "Msg": f"Current time in Moscow:"  # noqa: E501
-                                                             f"{datetime.now(tz=ZoneInfo('Europe/Moscow')).strftime(FORMAT)}",
-                                                      # noqa: E501
+                                                             f"{datetime.now(tz=ZoneInfo('Europe/Moscow')).strftime(FORMAT)}",  # noqa: E501
                                                       "Href": "/",
                                                       "LinkMsg": "Main page"},
                                              status_code=200)
--- app_python/templates/templates.py
+++ app_python/templates/templates.py
@@ -3,4 +3,3 @@ from starlette.templating import Jinja2Templates
 
 def SetUpTemplates(path):
     return Jinja2Templates(directory=path)
-
