eadc4480ef637570926de4fbfca7d6197edcb2f1
m4k4rich <m4k4rich@gmail.com>
2023-09-20 01:03:06 +0300

    fix: test_app.py & python_ci.yml

--- app_python/test_app.py
+++ app_python/test_app.py
@@ -43,10 +43,8 @@ class TestDisplayTime(unittest.TestCase):
 
         # Calculate the time difference
         time_difference = app_time - current_time_moscow
-        print("\nThe absolute time difference is: " \
+        print("\nThe absolute time difference is: " 
               + str(abs(time_difference)))
-        # Ensure that the time difference is within a reasonable range (e.g.,
-        # less than 5 seconds)
         self.assertLessEqual(
             abs(time_difference.total_seconds()), 5
         )
