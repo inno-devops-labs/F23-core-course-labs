308c1146ad8ff2106f6071b15cf1137d4178e413
yesliesnayder <kuzmickiy2000@mail.ru>
2023-09-19 21:32:06 +0300

    Fixed tests

--- app_python/tests/test_main.py
+++ app_python/tests/test_main.py
@@ -2,7 +2,7 @@ import pytest
 import time
 from datetime import datetime
 
-from app_python.src.config import time_format
+from app_python.src.config import TIME_FORMAT
 from app_python.tests.client import client
 
 
@@ -14,13 +14,13 @@ async def test_get_time(client):
     body = resp.json()
     assert "current_time" in resp.json(), f"Incorrect body: {body}"
 
-    t1 = datetime.strptime(body["current_time"], time_format)
+    t1 = datetime.strptime(body["current_time"], TIME_FORMAT)
     time.sleep(1)
 
     resp = await client.get("/")
     body = resp.json()
     assert "current_time" in resp.json(), f"Incorrect body for second request: {body}"
 
-    t2 = datetime.strptime(body["current_time"], time_format)
+    t2 = datetime.strptime(body["current_time"], TIME_FORMAT)
 
     assert t1 < t2, "Time is constant and not updating"
