6cc00b00d058fddb3ab282e9d7c1a6fce360a242
Nikita Pozdniakov <nikitosing4@mail.ru>
2023-09-19 20:59:03 +0300

    Lab 3: Check workflow

--- app_python/test/route/test_time.py
+++ app_python/test/route/test_time.py
@@ -1,4 +1,14 @@
-
 def test_request_time(client):
     response = client.get("time/")
-    assert b'Europe/Moscow' in response.data
+    assert response.json["time"]
+    assert response.json["timezone"] == 'Europe/Moscow'
+    assert response.status_code == 200
+
+
+def test_two_request(client):
+    response_1 = client.get("time/")
+    response_2 = client.get("time/")
+
+    assert response_2.json["timezone"] == response_1.json["timezone"]
+    assert response_1.json["timezone"] == 'Europe/Moscow'
+    assert response_1.json["time"] <= response_2.json["time"]
