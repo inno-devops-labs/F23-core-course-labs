cd8e0878e8a42840d323eb824a98bf1e5922b68a
Nikita Pozdniakov <nikitosing4@mail.ru>
2023-09-19 23:06:52 +0300

    Add CI for elixir

--- app_elixir/test/support/conn_case.ex
+++ app_elixir/test/support/conn_case.ex
@@ -31,8 +31,7 @@ defmodule DevopsWebWeb.ConnCase do
     end
   end
 
-  setup tags do
-    DevopsWeb.DataCase.setup_sandbox(tags)
+  setup _tags do
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
 end
