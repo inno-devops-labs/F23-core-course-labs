a1b63be6309543c2150479b60889ecb28bf180b7
Ignat Petrov <petrov-ignat85@mail.ru>
2023-09-19 22:57:25 +0300

    Add lab3

--- app_python/PYTHON.md
+++ app_python/PYTHON.md
@@ -7,3 +7,8 @@ And I've already worked with this.
 1. Use requirements.txt for list required packages. This makes it easier to get started with the application.
 2. "Keep it simple". Very simple and short code.
 3. Use very simple and fast ASGI - Uvicorn
+
+## Unit test best practices:
+1. Use a library for testing
+2. Use simple tests
+3. Craft deterministic tests
--- app_python/src/main.py
+++ app_python/src/main.py
@@ -1,14 +1,17 @@
+"""Main file to run"""
+from datetime import datetime
+
+import pytz
 import uvicorn
 from fastapi import FastAPI
-import pytz
-from datetime import datetime
 
 app = FastAPI()
 
 
 @app.get("/")
 async def home():
-    now = datetime.now(tz=pytz.timezone('Europe/Moscow'))
+    """Return Moscow time."""
+    now = datetime.now(tz=pytz.timezone("Europe/Moscow"))
 
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
     return dt_string
