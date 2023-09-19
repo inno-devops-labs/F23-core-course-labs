112a59e1e70a15d1e83c2033ebe27ad14ec777df
DyllasDek <d.korneenko@innopolis.university>
2023-09-19 22:14:40 +0300

    Update test_app.py to fit flake8

--- app_python/test_app.py
+++ app_python/test_app.py
@@ -4,24 +4,27 @@ import unittest
 from pytz import timezone
 from app import app
 
+
 class TestApp(unittest.TestCase):
-    #Set up test client
+    # Set up test client
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
 
     def test_time_page(self):
-        #Send GET request to server
+        # Send GET request to server
         response = self.app.get('/')
-        
-        #Get current time in Moscow
-        moscow_time = datetime.now(timezone('Europe/Moscow')).strftime('%Y-%m-%d %H:%M:%S')
-        
-        #Check if server returns OK status code
+
+        # Get current time in Moscow
+        moscow_time = datetime.now(
+            timezone('Europe/Moscow')).strftime('%Y-%m-%d %H:%M:%S')
+
+        # Check if server returns OK status codes
         self.assertEqual(response.status_code, 200)
-        #Check if server returns correct time
-        self.assertIn(f'Current time in Moscow: {moscow_time}', response.data.decode())
-        
-        
+        # Check if server returns correct time
+        self.assertIn(
+            f'Current time in Moscow: {moscow_time}', response.data.decode())
+
+
 if __name__ == '__main__':
     unittest.main()
