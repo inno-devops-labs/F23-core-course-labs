8c2d6b1768f445aced1e6c92a3cbdc7cea45cb9f
Mikhail Fedorov <fedorovm093@gmail.com>
2023-09-19 20:30:34 +0300

    feat: add unit tests

--- app_python/tests/test_msk_time.py
+++ app_python/tests/test_msk_time.py
@@ -1,9 +1,19 @@
-from fastapi.testclient import TestClient
+# tests/test_integration.py
+import pytest
+from httpx import AsyncClient
 from src.main import app
 
-client = TestClient(app)
+@pytest.mark.asyncio
+async def test_get_moscow_time():
+    async with AsyncClient(app=app, base_url="http://testserver") as client:
+        response = await client.get("/time")
+        assert response.status_code == 200
+        data = response.json()
+        assert "time" in data
 
-def test_get_moscow_time():
-    response = client.get("/moscow-time")
-    assert response.status_code == 200
-    assert "time" in response.json()
+@pytest.mark.asyncio
+async def test_invalid_timezone():
+    async with AsyncClient(app=app, base_url="http://testserver") as client:
+        response = await client.get("/time?timezone=InvalidTimezone")
+        assert response.status_code == 400
+        assert "detail" in response.json()
