c842096041e3a7db42f917c8dfd7eca1ea288662
MaximInnopolis <71584246+MaximInnopolis@users.noreply.github.com>
2023-09-20 00:54:35 +0300

    Added comments

--- app_python/tests/test_app.py
+++ app_python/tests/test_app.py
@@ -1,15 +1,33 @@
+"""
+This module contains unit tests for the Flask web application.
+"""
+
 from datetime import datetime
 import unittest
 from app import app, MOSCOW_TZ
 
-
 class TestTimeDisplay(unittest.TestCase):
+    """
+    Test class for the time display functionality of the web application.
+    """
 
     def setUp(self):
+        """
+        Set up the test environment before each test.
+        """
         test = app.test_client()
         self.test_display_time(test)
 
     def test_display_time(self, test):
+        """
+        Test the display_time function.
+
+        Sends a GET request to the app's root URL, retrieves the response,
+        and checks if it contains the current Moscow time.
+
+        Args:
+            test: The Flask test client.
+        """
         # Send a GET request to the app's root URL
         response = test.get('/', content_type='html/text')
 
@@ -21,7 +39,6 @@ class TestTimeDisplay(unittest.TestCase):
         # Check if time is true
         response_data_str = response.data.decode("utf-8")
         self.assertTrue(moscow_time_str in response_data_str)
-
 
 if __name__ == '__main__':
     unittest.main()
