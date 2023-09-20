121c91a9dfec3efe7fd07758a5e2aade33e62ce6
y.kim <kim.yaroslavik@gmail.com>
2023-09-20 07:35:53 +0300

    Lab 3 - Aligning with new linter requirements

--- app_python/flaskr/util_functions.py
+++ app_python/flaskr/util_functions.py
@@ -3,14 +3,14 @@ from datetime import datetime
 import pytz
 
 
-def get_date(timezone : str, format_str : str) -> str:
+def get_date(timezone: str, format_str: str) -> str:
     """Get current date in a specific timezone and format"""
     timezone_obj = pytz.timezone(timezone)
     time_str = datetime.now(timezone_obj).strftime(format_str)
     return time_str
 
 
-def str_to_datetime(datetime_str : str) -> datetime:
+def str_to_datetime(datetime_str: str) -> datetime:
     """Converting specifically formatted strings to datetime objects"""
 
     # handling some weirdness of response format.
@@ -26,7 +26,7 @@ def str_to_datetime(datetime_str : str) -> datetime:
     return datetime_obj
 
 
-def datetime_to_minutes(datetime_obj : datetime) -> int:
+def datetime_to_minutes(datetime_obj: datetime) -> int:
     """Convert datetime object to integer value of minutes since 0 A.D."""
     return (
         datetime_obj.year * 365 * 24 * 60 +
@@ -37,7 +37,7 @@ def datetime_to_minutes(datetime_obj : datetime) -> int:
     )
 
 
-def compare_dates(datetime_a : str, datetime_b : str) -> bool:
+def compare_dates(datetime_a: str, datetime_b: str) -> bool:
     """Check if 2 datetimes are no more than 2 minutes apart"""
     minutes_a = datetime_to_minutes(str_to_datetime(datetime_a))
     minutes_b = datetime_to_minutes(str_to_datetime(datetime_b))
