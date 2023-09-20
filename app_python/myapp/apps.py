21acf97f816906b9ec57b1355aefcd23693c09f0
k.tyulebaeva <k.tyulebaeva@tinkoff.ru>
2023-09-20 08:06:43 +0300

    lab3

--- app_python/myapp/apps.py
+++ app_python/myapp/apps.py
@@ -1,6 +1,8 @@
+"""App module"""
 from django.apps import AppConfig
 
-
 class MyappConfig(AppConfig):
+    """My App configuration"""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myapp'
--- app_python/myapp/views.py
+++ app_python/myapp/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
+"""views module"""
 from datetime import datetime, timezone, timedelta
+from django.http import HttpResponse
 
-def current_time(request):
+def current_time():
+    """returns current time"""
     moscow_tz = timezone(timedelta(hours=3))
     now = datetime.now(moscow_tz)
     return HttpResponse(now.strftime('%Y-%m-%d %H:%M:%S'))
-
-
+ 
