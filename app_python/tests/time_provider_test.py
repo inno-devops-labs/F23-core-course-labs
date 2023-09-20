5101395cb6b02d742df34f75cbeb8662c97c9540
vlasov-egor <vlasov-egorl@ya.ru>
2023-09-20 10:36:44 +0300

    fixed test

--- app_python/tests/time_provider_test.py
+++ app_python/tests/time_provider_test.py
@@ -1,11 +1,13 @@
-from src.time_provider import TimeProvider
-from datetime import datetime
+import pytz
 from time import sleep
+from datetime import datetime
+
+from src.time_provider import TimeProvider
 
 
 def test_get_current_time():
-    assert TimeProvider.get_current_datetime().strftime(
-        '%H:%M:%S') == datetime.now().strftime('%H:%M:%S')
+    assert TimeProvider.get_current_datetime().strftime('%H:%M:%S') == datetime.now(
+        tz=pytz.timezone('Europe/Moscow')).strftime('%H:%M:%S')
     sleep(3)
-    assert TimeProvider.get_current_datetime().strftime(
-        '%H:%M:%S') == datetime.now().strftime('%H:%M:%S')
+    assert TimeProvider.get_current_datetime().strftime('%H:%M:%S') == datetime.now(
+        tz=pytz.timezone('Europe/Moscow')).strftime('%H:%M:%S')
