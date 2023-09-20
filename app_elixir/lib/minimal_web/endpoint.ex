48524ae283e9a7ba3648c3d44abc5552ca65ba5b
Maxim Filonov <53992153+sl1depengwyn@users.noreply.github.com>
2023-09-20 13:09:58 +0300

    Done for elixir

--- app_elixir/lib/minimal_web/endpoint.ex
+++
@@ -1,5 +0,0 @@
-defmodule MinimalWeb.Endpoint do
-  use Phoenix.Endpoint, otp_app: :minimal
-
-  plug(MinimalWeb.Router)
-end
--- app_elixir/lib/minimal_web/views/error_view.ex
+++
@@ -1,5 +0,0 @@
-defmodule MinimalWeb.ErrorView do
-  def render("404.html", _assigns) do
-    "Not Found"
-  end
-end
