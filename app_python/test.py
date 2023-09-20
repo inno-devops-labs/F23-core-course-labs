703c72b0e6cc18067e0e764d5bae0339d1ec4722
beleet <105719971+beleet@users.noreply.github.com>
2023-09-20 11:13:52 +0300

    Update test.py

--- app_python/test.py
+++ app_python/test.py
@@ -8,23 +8,22 @@ from .config import timezone
 
 @pytest.fixture
 def test_client():
-
-	return get_test_client()
+    return get_test_client()
 
 
 def test_request(test_client):
 
-	response = test_client.get('/')
+    response = test_client.get('/')
 
-	assert response.status_code == 200
+    assert response.status_code == 200
 
-	moscow_tz = pytz.timezone(zone=timezone)
+    moscow_tz = pytz.timezone(zone=timezone)
 
-	soup = BeautifulSoup(response.text, 'html.parser')
-	response_time = soup.find('p').text
-	response_time = datetime.datetime.strptime(response_time, '%H:%M:%S')
-	response_time = moscow_tz.localize(response_time)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    response_time = soup.find('p').text
+    response_time = datetime.datetime.strptime(response_time, '%H:%M:%S')
+    response_time = moscow_tz.localize(response_time)
 
-	current_time = datetime.datetime.now(moscow_tz)
+    current_time = datetime.datetime.now(moscow_tz)
 
-	assert response_time - current_time < datetime.timedelta(seconds=5)
+    assert response_time - current_time < datetime.timedelta(seconds=5)
