e5ab2397df7dc57e11dc98194f123919d6e08c94
Mikhail Fedorov <fedorovm093@gmail.com>
2023-09-20 08:39:43 +0300

    feat: update linting

--- app_python/src/routes/index.py
+++ app_python/src/routes/index.py
@@ -1,3 +1,6 @@
+"""
+This file defines the routes for the root URL ("/") and the static files
+"""
 from fastapi import APIRouter
 from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -6,7 +9,8 @@ router = APIRouter()
 
 router.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Define a route for the root URL ("/") to serve index.html
+
 @router.get("/", response_class=HTMLResponse)
 async def root():
+    """ Define a route for the root URL ("/") to serve index.html """
     return FileResponse("static/index.html")
