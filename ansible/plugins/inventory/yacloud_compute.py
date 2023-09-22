e6404adf709a74989957636be41579983bbf632d
Dzhemilya Vechtomova <d.vechtomova@innopolis.university>
2023-09-22 18:55:33 +0300

    fpre-commit

--- ansible/plugins/inventory/yacloud_compute.py
+++ ansible/plugins/inventory/yacloud_compute.py
@@ -1,4 +1,5 @@
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+ (see COPYING or
+# https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import (absolute_import, division, print_function)
 from ansible.module_utils._text import to_native
@@ -40,7 +41,7 @@ DOCUMENTATION = '''
             description: VM's label used for group assignment
             type: string
             default: ""
-            
+
 '''
 
 EXAMPLES = '''
