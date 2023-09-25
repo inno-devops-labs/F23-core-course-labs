76f687005d262b5757c55920c535330886750ddd
i.nafikov <i.nafikov@tinkoff.ru>
2023-09-26 02:21:57 +0300

    feat: complete lab4 main and bonus task
    Closes: lab3-main, lab3-bonus

--- terraform/github/main.tf
+++ terraform/github/main.tf
@@ -1,9 +1,10 @@
 provider "github" {
   token = var.token
+  owner = var.github_organization
 }
 
-resource "github_repository" "iu-devops-course-2" {
-  name             = "iu-devops-course-2"
+resource "github_repository" "iu-devops-course-3" {
+  name             = "iu-devops-course-3"
   description      = "Assignments for DevOps Course at Innopolis University"
   visibility       = "public"
   has_issues       = true
@@ -14,13 +15,13 @@ resource "github_repository" "iu-devops-course-2" {
 
 # Set default branch to "main"
 resource "github_branch_default" "main" {
-  repository = github_repository.iu-devops-course-2.name
+  repository = github_repository.iu-devops-course-3.name
   branch     = "main"
 }
 
 # Create branch protection rule to protect the default branch
 resource "github_branch_protection" "default" {
-  repository_id                   = github_repository.iu-devops-course-2.id
+  repository_id                   = github_repository.iu-devops-course-3.id
   pattern                         = github_branch_default.main.branch
   require_conversation_resolution = true
   enforce_admins                  = true
@@ -41,13 +42,13 @@ resource "github_team" "team2" {
 }
 
 resource "github_team_repository" "team1_repo" {
-  team_id = github_team.team1.id
-  repository = github_repository.iu-devops-course-2.name
+  team_id    = github_team.team1.id
+  repository = github_repository.iu-devops-course-3.name
   permission = "push"
 }
 
 resource "github_team_repository" "team2_repo" {
-  team_id = github_team.team2.id
-  repository = github_repository.iu-devops-course-2.name
+  team_id    = github_team.team2.id
+  repository = github_repository.iu-devops-course-3.name
   permission = "admin"
 }
