8e2159a7cf4325055e14a6ad33530762a11ae84b
ritasidorska <m.sidorskaya@innopolis.university>
2023-09-26 23:10:53 +0300

    Added terraform github configuration

---
+++ terraform/github/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {
+  token = var.github_token
+}
+
+resource "github_repository" "repo" {
+  name        = "core-course-labs"
+  description = "My labs repository"
+  visibility  = "public"
+}
+
+#Set default branch 'master'
+resource "github_branch_default" "main" {
+  repository = github_repository.repo.name
+  branch     = "main"
+}
+
+#Create branch protection rule to protect the default branch. (Use "github_branch_protection_v3" resource for Organisation rules)
+resource "github_branch_protection" "default" {
+  repository_id                   = github_repository.repo.id
+  pattern                         = github_branch_default.main.branch
+  require_conversation_resolution = true
+  enforce_admins                  = true
+
+  required_pull_request_reviews {
+    required_approving_review_count = 1
+  }
+}
