c4274bf99c26d42ddc33e63e98e2238af4df3c09
aibek.bakirov <aibek.bakirov99@gmail.com>
2023-09-26 04:15:55 +0300

    commit unit tests correction

--- app_python/tests/test_app.py
+++ app_python/tests/test_app.py
@@ -1,5 +1,6 @@
 import pytest
 from app import app
+import time
 
 
 @pytest.fixture
@@ -8,8 +9,21 @@ def client():
         yield client
 
 
-def test_index(client):
+def test_content_of_page(client):
     response = client.get("/")
     assert b"Current Time in Moscow:" in response.data
     assert b"Date:" in response.data
     assert b"Time:" in response.data
+
+
+def test_time_updates_on_refresh(client):
+    response = client.get("/")
+    initial_time = extract_time_from_response(response)
+    time.sleep(2)
+    response = client.get("/")
+    refreshed_time = extract_time_from_response(response)
+    assert initial_time != refreshed_time
+
+
+def extract_time_from_response(response):
+    return response.get_data(as_text=True).strip()
