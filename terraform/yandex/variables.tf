5bd447e2fc49481f612f6bc254b3e40c85511ee0
lcensies <0xf1o2732@proton.me>
2023-09-27 02:31:49 +0300

    parametrized yandex container images

--- terraform/yandex/variables.tf
+++ terraform/yandex/variables.tf
@@ -8,7 +8,6 @@ variable "yandex_token" {
 variable "cloud_id" {
   type        = string
   description = "ID of the cloud workspace"
-  sensitive   = true
 }
 
 # https://cloud.yandex.com/en/docs/resource-manager/operations/folder/get-id
@@ -24,3 +23,14 @@ variable "service_account_id" {
   sensitive   = true
 }
 
+variable "image_url" {
+	type = string
+	description = "URL of the Docker image including the container registry"
+	default = "cr.yandex/crpsmrhh2qi32dnr42gk/nginx:1.25.2-alpine3.18"
+}
+
+variable "image_digest" {
+	type = string
+	description = "Hash of the Docker image"
+	default = "sha256:433dbc17191a7830a9db6454bcc23414ad36caecedab39d1e51d41083ab1d629"	
+}
