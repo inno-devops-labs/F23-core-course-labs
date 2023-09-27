c816ff5835a03d4dd865cfc3c7c5064a210bbced
wareverdud <rkhakimov1337@gmail.com>
2023-09-27 07:26:43 +0300

    fix

--- terraform/github/variables.tf
+++ terraform/github/variables.tf
@@ -2,5 +2,4 @@ variable "token" {
   type        = string
   description = "Specifies the `GITHUB_TOKEN`"
   sensitive   = true
-  default = "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"
 }
--- terraform/yandex/variables.tf
+++ terraform/yandex/variables.tf
@@ -1,17 +1,14 @@
 variable "cloud_id" {
   description = "cloud id"
   type        = string
-  default     = "b1g9ocvhhbkbcpg3dsfv"
 }
 
 variable "folder_id" {
   description = "folder id"
   type        = string
-  default     = "b1gccr3oloqlg8l4ctie"
 }
 
 variable "token" {
   description = "token path"
   type        = string
-  default     = "./authorized_key.json"
 }
