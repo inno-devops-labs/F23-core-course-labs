88cba2e7afba19dd59150d46a16b0b1231877f40
Nikita Pozdniakov <nikitosing4@mail.ru>
2023-10-04 10:59:19 +0300

    fix

--- ansible/inventory/script.sh
+++ ansible/inventory/script.sh
@@ -2,5 +2,4 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TERRAFORM_INVENTORY=`which terraform-inventory`
 TF_STATE="$CURRENT_DIR/../../terraform/cloud"    # <<== relative path to dir with .tfstate
-echo $TF_STATE
 "$TERRAFORM_INVENTORY" "$@" "$TF_STATE"
