9fd8acbdba29b8712c9c8e39595900d505e300b9
Anatoliy <shvarts.tolik@mail.ru>
2023-10-05 23:39:16 +0300

    Update python env config to use defaults

--- app_python/src/config.py
+++ app_python/src/config.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config:
-    host = os.getenv("SERVER_HOST")
-    port = int(os.getenv("SERVER_PORT"))
+    host = os.getenv("SERVER_HOST", default="0.0.0.0")
+    port = int(os.getenv("SERVER_PORT", default=8080))
 
 
 config = Config()
