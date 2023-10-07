12ae072ec199bf000cca3a893fe88c31602e84a4
yesliesnayder <kuzmickiy2000@mail.ru>
2023-10-07 23:36:50 +0300

    Added user to vm

--- terraform/yandex-cloud/main.tf
+++ terraform/yandex-cloud/main.tf
@@ -11,11 +11,11 @@ provider "yandex" {
   token     = var.token
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
-  zone = "ru-central1-a"
+  zone      = "ru-central1-a"
 }
 
 data "yandex_compute_image" "last_ubuntu" {
-  family = "ubuntu-2204-lts"  # ОС (Ubuntu, 22.04 LTS)
+  family = "ubuntu-2204-lts" # ОС (Ubuntu, 22.04 LTS)
 }
 
 data "yandex_vpc_subnet" "default_a" {
@@ -27,8 +27,8 @@ resource "yandex_compute_instance" "vm_1" {
 
   resources {
     core_fraction = 5
-    cores  = 2 # vCPU
-    memory = 1 # RAM
+    cores         = 2 # vCPU
+    memory        = 1 # RAM
   }
 
   boot_disk {
@@ -39,7 +39,13 @@ resource "yandex_compute_instance" "vm_1" {
 
   network_interface {
     subnet_id = data.yandex_vpc_subnet.default_a.subnet_id
-    nat = true
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys  = "ubuntu:${file("~/.ssh/ya_cloud.pub")}"
+    user-data = "${file("./meta.txt")}"
+    serial-port-enable    = 1
   }
 }
 
@@ -48,8 +54,8 @@ resource "yandex_compute_instance" "vm_2" {
 
   resources {
     core_fraction = 5
-    cores  = 2
-    memory = 1
+    cores         = 2
+    memory        = 1
   }
 
   boot_disk {
@@ -60,6 +66,12 @@ resource "yandex_compute_instance" "vm_2" {
 
   network_interface {
     subnet_id = data.yandex_vpc_subnet.default_a.subnet_id
-    nat = true
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys  = "ubuntu:${file("~/.ssh/ya_cloud.pub")}"
+    user-data = "${file("./meta.txt")}"
+    serial-port-enable    = 1
   }
 }
