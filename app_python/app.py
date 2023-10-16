fbc365852315bb89d0c8a36bd874399cd13447a9
AnnaDluzhinskaya <a.dluzhinskaya@gmail.com>
2023-10-16 17:13:28 +0300

    Updated python application

--- app_python/app.py
+++ app_python/app.py
@@ -2,6 +2,15 @@ from datetime import datetime
 from flask import Flask, render_template
 import pytz
 from waitress import serve
+import logging
+
+logging.basicConfig(
+    format='%(levelname)-8s -- %(asctime)-s -- Message: \t%(message)s',
+    datefmt='%Y-%m-%dT%H:%M:%S'
+)
+
+logger = logging.getLogger("waitress")
+logger.setLevel(logging.INFO)
 
 
 # Функция, которая возвращает время в определенном месте (в формате %H:%M:%S)
@@ -16,6 +25,7 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     current_time = get_timezone('Europe/Moscow')
+    logger.info(msg=f'Route: /')
     return render_template('index.html', current_time=current_time)
 
 
