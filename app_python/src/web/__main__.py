1e41d222e4a69f6d475a798fe5c1dcc79b436df8
elestrias <rus8-2002@mail.ru>
2023-10-17 15:05:32 +0300

    Fix app_python logging

--- app_python/src/web/__main__.py
+++ app_python/src/web/__main__.py
@@ -1,4 +1,5 @@
 from aiohttp import web
+import logging
 
 from src.config import Config
 from src.web.app import init_app
@@ -7,5 +8,5 @@ from src.web.app import init_app
 if __name__ == '__main__':
     config = Config()
     app = init_app()
-
+    logging.basicConfig(level=logging.DEBUG)
     web.run_app(app=app, host='0.0.0.0', port=config.PORT)
