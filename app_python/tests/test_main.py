22ea615bb5e526e68a1e4995fea9f90e74c5bc7b
kerniee <karapys.mor@gmail.com>
2023-10-19 11:35:37 +0300

    fix tests and formatting lab8

--- app_python/tests/test_main.py
+++ app_python/tests/test_main.py
@@ -1,14 +1,9 @@
-import pytest
-
-
-@pytest.mark.anyio
-async def test_get_time(async_app_client):
-    response = await async_app_client.get("/")
+def test_get_time(app_client):
+    response = app_client.get("/")
     assert response.status_code == 200
     assert "time" in response.json()
 
 
-@pytest.mark.anyio
-async def test_invalid_path(async_app_client):
-    response = await async_app_client.get("/invalid")
+def test_invalid_path(app_client):
+    response = app_client.get("/invalid")
     assert response.status_code == 404
