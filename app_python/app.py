18971ed7078628852d037dce664f6317ca9e619c
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-10-22 15:01:02 +0300

    add prometheus dashboard for monitoring

--- app_python/app.py
+++ app_python/app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import datetime
 import pytz
+from prometheus_client import Counter, generate_latest, CollectorRegistry
 
 app = Flask(__name__)
 
+# Create Prometheus metrics
+request_counter = Counter('http_requests_total', 'Total number of requests to the app')
 
 def get_moscow_time():
     moscow_timezone = pytz.timezone("Europe/Moscow")
@@ -13,6 +16,7 @@ def get_moscow_time():
 
 @app.route("/")
 def show_time():
+    request_counter.inc()
     moscow_time = get_moscow_time()
     return render_template("index.html", moscow_time=moscow_time)
 
@@ -23,6 +27,15 @@ def get_time():
     moscow_time = datetime.datetime.now(moscow_timezone)
     formatted_time = moscow_time.strftime("%Y-%m-%d %H:%M:%S")
     return formatted_time
+
+
+
+@app.route('/metrics')
+def metrics():
+    registry = CollectorRegistry()
+    data = generate_latest(registry)
+    return Response(data, content_type='text/plain; version=0.0.4')
+    
 
 
 if __name__ == "__main__":
