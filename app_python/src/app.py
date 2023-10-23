75ff99ba1046801100aa940d0bf06296cc2ccfa4
teawd <52975098+TeaWhyDee@users.noreply.github.com>
2023-10-23 18:54:25 +0300

    lab 8: bonus

--- app_python/src/app.py
+++ app_python/src/app.py
@@ -4,15 +4,20 @@ The website displays current time.
 """
 
 from datetime import datetime
+
 import pytz
 from flask import Flask
+from prometheus_client import Summary
 
 app = Flask(__name__)
 
-
 CITY = {"text": "Moscow", "exact": "Europe/Moscow"}
 
+# Prometheus metrics
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 
+
+@REQUEST_TIME.time()
 @app.route("/")
 def index() -> str:
     """Return current time to an HTTP request."""
@@ -25,4 +30,5 @@ def index() -> str:
 
 
 if __name__ == "__main__":
+    # Flask server
     app.run(host="0.0.0.0", port=5000, threaded=True)
