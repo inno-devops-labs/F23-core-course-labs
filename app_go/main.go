e569962d646a18511c0b9eddca9e24574e25af96
Artem Murashko <art22m@yandex-team.ru>
2023-10-24 22:28:46 +0300

    add metrics to goapp

--- app_go/main.go
+++ app_go/main.go
@@ -5,12 +5,26 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	timeHandlerProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "time_handler_processed",
+		Help: "The total number of time handler calls",
+	})
 )
 
 const timeFormat = "2006-01-02 15:04:05"
 
 func currentMoscowTimeHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("GET / HTTP")
+
+	timeHandlerProcessed.Inc()
+
 	location, err := time.LoadLocation("Europe/Moscow")
 	if err != nil {
 		log.Println(err)
@@ -22,6 +36,7 @@ func currentMoscowTimeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/", currentMoscowTimeHandler)
 	if err := http.ListenAndServe(":9000", nil); err != nil {
 		log.Fatal(err)
