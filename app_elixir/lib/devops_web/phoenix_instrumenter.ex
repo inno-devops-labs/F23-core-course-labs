661fb22003d74d46b5e904e411e831f2266695df
Nikita Pozdniakov <nikitosing4@mail.ru>
2023-10-24 22:03:06 +0300

    Done lab8 with bonus task

---
+++ app_elixir/lib/devops_web/phoenix_instrumenter.ex
@@ -0,0 +1,6 @@
+defmodule DevopsWeb.PhoenixInstrumenter do
+  @moduledoc """
+  Provides instrumentation for Phoenix specific metrics.
+  """
+  use Prometheus.PhoenixInstrumenter
+end
--- app_python/app.py
+++ app_python/app.py
@@ -1,14 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify
 from api.route.time import time_blueprint
 from flasgger import Swagger
-
+from prometheus_client import start_http_server, Counter
 
 def init() -> Flask:
     app = Flask(__name__)
     app.register_blueprint(time_blueprint, url_prefix='/time')
     return app
 
-
 app = init()
 
 swagger = Swagger(app)
+
+
+@app.route('/metrics')
+def metrics():
+    from prometheus_client.exposition import generate_latest
+    return generate_latest()
+
+request_counter = Counter('myapp_requests_total', 'Total number of requests to /')
+
+# Function to increment the request counter
+def increment_request_counter():
+    request_counter.inc()
+
+# Define a sample route
+@app.route('/')
+def hello():
+    increment_request_counter()  # Increment the request counter
+    return jsonify(message="Hello, World!")
