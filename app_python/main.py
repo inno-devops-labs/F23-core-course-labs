6608d925c03129639f2731d5ae2f11726eb117da
Doldrums <doldrums.317@gmail.com>
2023-10-26 11:35:47 +0300

    feat: lab8

--- app_python/main.py
+++ app_python/main.py
@@ -2,12 +2,14 @@ import os
 from pathlib import Path
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from .routers import time
 
 STATIC_PATH = Path(os.path.dirname(os.path.realpath(__file__))) / "static"
 
 app = FastAPI()
+Instrumentator().instrument(app).expose(app)
 
 app.include_router(time.router)
 
