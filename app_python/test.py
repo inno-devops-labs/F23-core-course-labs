1d9a996c3e691a4bb0d009d95353b64aa523033f
unknown <v.zelenokor@innopolis.university>
2023-11-15 11:30:48 +0300

    lab7 fixed

--- app_python/test.py
+++ app_python/test.py
@@ -1,34 +1,24 @@
-from application import app
-import unittest
+import pytest
 import time
 
-
-class TestApp(unittest.TestCase):
-
-    # initialize
-    def start(self):
-        self.app = app.test_client()
-
-    # test url link for work
-    def if_url(self):
-        resp = self.app.get("/")
-        self.assertEqual(resp.status_code, 200)
-
-    # check if time updates on every response
-    def if_updates(self):
-        # send first try
-        try_1 = self.app.get("/")
-        # wait
-        time.sleep(5)
-        # send second try
-        try_2 = self.app.get("/")
-        
-        result_1 = try_1.get_data(as_text=True)
-        result_2 = try_2.get_data(as_text=True)
-
-        # compare
-        self.assertNotEqual(result_1, result_2)
+from app_python.app import app
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture
+def client():
+    app.config.update({"TESTING": True})
+
+    with app.test_client() as client:
+        yield client
+
+ 
+def test_get_response(client):
+    response = client.get("/")
+    assert response.status_code == 200
+
+    
+def test_time_changed(client):
+    time1 = client.get("/").data.decode('utf-8')
+    time.sleep(1)
+    time2 = client.get("/").data.decode('utf-8')
+    assert time1 != time2
