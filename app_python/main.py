b39ef82c8732a254b9b680fdf063c09b724868e2
unknown <v.zelenokor@innopolis.university>
2023-11-15 20:54:56 +0300

    app_python fixed

--- app_python/main.py
+++ app_python/main.py
@@ -1,6 +1,18 @@
-from application import app
+from flask import Flask
+from datetime import datetime
+import pytz
+from waitress import serve
+from prometheus_flask_exporter import PrometheusMetrics
 
-app = app()
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+@app.route('/')
+def moscow_time():
+    timezone = pytz.timezone('Europe/Moscow')
+    time = datetime.now(timezone)
+    formatted_time = time.strftime('%Y-%m-%d, %H:%M:%S.%f')[:-3]
+    return 'Current Moscow time is ' + formatted_time
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    serve(app=app, port="5000")
