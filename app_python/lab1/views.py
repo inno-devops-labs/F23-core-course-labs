7d76a3e46bff83e38139043f28411dd071efe2b7
Klemencya <m.sidorskaya@innopolis.university>
2023-11-19 13:34:18 +0300

    Added persistence /visits endpoint to python_app

--- app_python/lab1/views.py
+++ app_python/lab1/views.py
@@ -5,4 +5,17 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse(str(datetime.now(tz=ZoneInfo("Europe/Moscow"))))
+    return HttpResponse("time: " + str(datetime.now(tz=ZoneInfo("Europe/Moscow"))))
+
+
+def visits(request):
+    with open('visit-counter.txt', 'r+') as file:
+        try:
+            visits_count = int(file.readline().strip())
+        except Exception:
+            visits_count = 0
+        visits_count += 1
+        file.seek(0)
+        file.write(str(visits_count))
+        file.truncate()
+        return HttpResponse(str(visits_count))
