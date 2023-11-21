1961a57829f2faecef3f5d5adee045216107eb29
Klemencya <m.sidorskaya@innopolis.university>
2023-11-21 23:25:03 +0300

    Fixed Kotlin linted tab character issues

--- app_kotlin/lab1/src/main/kotlin/ru/msidorskaya/lab1/controller/PageCounterController.kt
+++ app_kotlin/lab1/src/main/kotlin/ru/msidorskaya/lab1/controller/PageCounterController.kt
@@ -7,38 +7,38 @@ import java.io.File
 
 @Controller
 class PageCounterController {
-	init {
-		launchNumber = 1
-		totalLaunchNumber = try {
-			File(FILE_NAME).readText().toLong()
-		} catch (ex: Exception) {
-			0L // If the file is not created, or it contains unparseable Long, the count starts from 0
-		}
-	}
+    init {
+        launchNumber = 1
+        totalLaunchNumber = try {
+            File(FILE_NAME).readText().toLong()
+        } catch (ex: Exception) {
+            0L // If the file is not created, or it contains unparseable Long, the count starts from 0
+        }
+    }
 
-	@ResponseBody
-	@GetMapping("/")
-	fun getCurrentOpenNumber() =
-		"Воу, с момента последнего рестарта приложения Вы открывали эту страницу уже $launchNumber раз!"
-			.also {
-				launchNumber += 1
-			}
+    @ResponseBody
+    @GetMapping("/")
+    fun getCurrentOpenNumber() =
+        "Воу, с момента последнего рестарта приложения Вы открывали эту страницу уже $launchNumber раз!"
+            .also {
+                launchNumber += 1
+            }
 
-	@ResponseBody
-	@GetMapping("/visits")
-	fun getTotalVisitsCount(): String {
-		totalLaunchNumber += 1
+    @ResponseBody
+    @GetMapping("/visits")
+    fun getTotalVisitsCount(): String {
+        totalLaunchNumber += 1
 
-		val file = File(FILE_NAME)
-		file.writeText(totalLaunchNumber.toString())
+        val file = File(FILE_NAME)
+        file.writeText(totalLaunchNumber.toString())
 
-		return totalLaunchNumber.toString()
-	}
+        return totalLaunchNumber.toString()
+    }
 
-	companion object {
-		var launchNumber = 1
-		var totalLaunchNumber = 0L
+    companion object {
+        var launchNumber = 1
+        var totalLaunchNumber = 0L
 
-		private const val FILE_NAME = "visit-counter.txt"
-	}
+        private const val FILE_NAME = "visit-counter.txt"
+    }
 }
