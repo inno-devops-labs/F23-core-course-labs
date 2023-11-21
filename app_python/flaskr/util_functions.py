22677ae9d7bfe8a1fbe5e4ffdfaf5882f4fb49f5
y.kim <kim.yaroslavik@gmail.com>
2023-11-22 01:19:47 +0300

    Lab 12 Task 1-2 - catch empty visits case

--- app_python/flaskr/util_functions.py
+++ app_python/flaskr/util_functions.py
@@ -6,7 +6,10 @@ import pytz
 def get_date(timezone: str, format_str: str) -> str:
     """Get current date in a specific timezone and format"""
     with open('./volume/visits', 'r') as f:
-        visits = int(f.read()) + 1
+        s_visits = f.read()
+        if (s_visits == ''):
+            s_visits = '0'
+        visits = int(s_visits) + 1
     with open('./volume/visits', 'w') as f:
         f.write(str(visits))
     timezone_obj = pytz.timezone(timezone)
