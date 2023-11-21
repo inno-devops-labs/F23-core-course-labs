73f399da41ad814da25a7d366132775dd5533b0e
Ignat Petrov <petrov-ignat85@mail.ru>
2023-11-21 22:40:21 +0300

    Add lab12

--- app_python/src/main.py
+++ app_python/src/main.py
@@ -4,8 +4,33 @@ from datetime import datetime
 import pytz
 import uvicorn
 from fastapi import FastAPI
+from os.path import exists
+
+VISIT_FILE = "data/visits.txt"
 
 app = FastAPI()
+
+def update_visit():
+    if exists(VISIT_FILE):
+        with open(VISIT_FILE, "r") as file:
+            visits = int(file.read())
+    else:
+        visits = 0
+    with open(VISIT_FILE, "w") as file:
+        file.write(str(visits + 1))
+
+
+def get_visits():
+    if exists(VISIT_FILE):
+        with open(VISIT_FILE, "r") as file:
+            return int(file.read())
+    else:
+        return 0
+    
+
+@app.get("/visits")
+async def visits():
+    return get_visits()
 
 
 @app.get("/")
@@ -14,6 +39,7 @@ async def home():
     now = datetime.now(tz=pytz.timezone("Europe/Moscow"))
 
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    update_visit()
     return dt_string
 
 
