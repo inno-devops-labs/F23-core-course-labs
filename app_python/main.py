8b1f27b23681b2f3461a4f9e32afe310db2a8e4c
Doldrums <doldrums.317@gmail.com>
2023-11-21 12:49:21 +0300

    feat: lab12

--- app_python/main.py
+++ app_python/main.py
@@ -5,6 +5,7 @@ from fastapi.staticfiles import StaticFiles
 from prometheus_fastapi_instrumentator import Instrumentator
 
 from .routers import time
+from .routers import visits
 
 STATIC_PATH = Path(os.path.dirname(os.path.realpath(__file__))) / "static"
 
@@ -12,5 +13,6 @@ app = FastAPI()
 Instrumentator().instrument(app).expose(app)
 
 app.include_router(time.router)
+app.include_router(visits.router)
 
 app.mount("/", StaticFiles(directory=STATIC_PATH, html=True), name="static")
--- app_python/routers/time.py
+++ app_python/routers/time.py
@@ -5,7 +5,18 @@ from ..schemas.time import NowTime
 
 router = APIRouter(prefix='/api/time')
 
+
 @router.get('/now', response_model=NowTime)
 def get_now():
     '''Returns current time in Moscow'''
+
+    visits = 0
+    try:
+        with open("/persistence/visits", "r") as f:
+            visits = int(f.read())
+    except:
+        pass
+    with open("/persistence/visits", "w") as f:
+        f.write(str(visits + 1))
+
     return NowTime(timestamp=datetime.utcnow() + timedelta(hours=3))
