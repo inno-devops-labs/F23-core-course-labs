a13f9e5aabb818a6fdae95496f721425f8999f52
Markuu-s <qwert11223381@gmail.com>
2023-11-21 19:06:59 +0300

    Lab12.

--- app_python/app/__init__.py
+++ app_python/app/__init__.py
@@ -3,10 +3,31 @@ import datetime
 import pytz
 
 app = Flask(__name__)
+filename = './volume/counter.txt'
+
+def get_counter():
+    try:
+        with open(filename, "r") as file:
+            return file.read()
+    except FileNotFoundError:
+        with open(filename, "w") as file:
+            file.write('0')
+
+    return 0
+
+def inc():
+    counter = int(get_counter()) + 1
+    with open(filename, "w") as file:
+        file.write(str(counter))
+
+    return counter
+
 
 
 @app.route('/')
 def index():
+    inc()
+
     utc_now = datetime.datetime.now(pytz.utc)
     moscow_tz = pytz.timezone('Europe/Moscow')
     moscow_now = utc_now.astimezone(moscow_tz)
@@ -14,6 +35,9 @@ def index():
 
     return moscow_time
 
+@app.route('/visits')
+def counter():
+    return str(get_counter())
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(host='0.0.0.0', port=8080)
