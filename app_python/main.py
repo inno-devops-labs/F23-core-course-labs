c47334f9de0e47894f7d0068cbec29fc1d2df08c
unknown <v.zelenokor@innopolis.university>
2023-11-21 14:00:28 +0300

    app_python

--- app_python/main.py
+++ app_python/main.py
@@ -1,6 +1,15 @@
-from application import app
+from flask import Flask
+from datetime import datetime
+import pytz
 
-app = app()
+app = Flask(__name__)
+
+@app.route('/')
+def moscow_time():
+    timezone = pytz.timezone('Europe/Moscow')
+    time = datetime.now(timezone)
+    formatted_time = time.strftime('%Y-%m-%d, %H:%M:%S.%f')[:-3]
+    return 'Current Moscow time is ' + formatted_time
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host='127.0.0.1', debug=True)
