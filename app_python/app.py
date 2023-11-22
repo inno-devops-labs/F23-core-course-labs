950f5fc84ec5e4ce809bb73975a4450d6d20ec34
DyllasDek <d.korneenko@innopolis.university>
2023-11-22 03:40:35 +0300

    forget to stage app.py

--- app_python/app.py
+++ app_python/app.py
@@ -2,14 +2,40 @@ from flask import Flask
 from datetime import datetime
 from prometheus_flask_exporter import PrometheusMetrics
 import pytz
+import os
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
+path = '/data/visits.txt'
+
+def get_current_visits():
+    try:
+        if os.path.exists(path):
+            with open(path, 'r') as file:
+                return int(file.read())
+    except Exception as e:
+        print(e)
+    return 0
+
+def update_visits():
+    try:
+        current_visits = get_current_visits()
+        if os.path.exists(path):
+            with open(path, 'w') as file:
+                file.write(str(current_visits + 1))
+    except Exception as e:
+        print(e)
+    return 0
+
+@app.route("/visits")
+def visits():
+    return str(get_current_visits())
 
 
 @app.route('/')
 # Get current time in Moscow
 def current_time_moscow():
+    update_visits()
     # Set timezone
     moscow_tz = pytz.timezone('Europe/Moscow')
     # Get current time in Moscow
