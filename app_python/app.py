1bd200e3c1fb1763a7eeec1d00dddeb3a1b07eef
dmitriypru <dmitriypru@gmail.com>
2023-11-22 14:14:59 +0300

    Added lab12 solution

--- app_python/app.py
+++ app_python/app.py
@@ -1,13 +1,39 @@
 from fastapi import FastAPI
 from datetime import datetime
+import os
 import pytz
 from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 Instrumentator().instrument(app).expose(app)
+path = '/data/visits.txt'
+
+def get_current_visits():
+    try:
+        if os.path.exists(path):
+            with open(path, 'r') as file:
+                return int(file.read())
+    except Exception as e:
+        print(e)
+    return 0
+
+def update_visits():
+    try:
+        current_visits = get_current_visits()
+        if os.path.exists(path):
+            with open(path, 'w') as file:
+                file.write(str(current_visits + 1))
+    except Exception as e:
+        print(e)
+    return 0
+
+@app.get('/visits')
+def get_visits():
+    return str(get_current_visits())
 
 @app.get('/api/time')
 def show_time():
+    update_visits()
     """
     Get current time in Moscow in '%Y-%m-%d %H:%M:%S format.
 
