0ea3a1dbce4c1e5a5687fe8e504f1804f483c491
Kopeikina Anna <kopkopkop47@gmail.com>
2023-11-22 10:41:26 +0300

    lab12

--- app_python/app.py
+++ app_python/app.py
@@ -2,14 +2,41 @@
 
 import datetime
 import pytz
+import os
 from flask import Flask, render_template
 from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
+path = '/data/visits.txt'
+
+def get_current_visits():
+    try:
+        if os.path.exists(path):
+            with open(path, 'r') as file:
+                return int(file.read())
+    except Exception as e:
+        print(e)
+    return 0
+
+@app.route("/visits")
+def visits():
+    return str(get_current_visits())
+
+def update_visits():
+    try:
+        current_visits = get_current_visits()
+        if os.path.exists(path):
+            with open(path, 'w') as file:
+                file.write(str(current_visits + 1))
+    except Exception as e:
+        print(e)
+    return 0
+
 @app.route('/')
 def index():
+    update_visits()
     """Get current time in GMT+3 timezone."""
     moscow = pytz.timezone('Europe/Moscow')
     # Format time to YYYY-MM-DD HH:MM:SS
