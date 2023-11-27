b3bb9d8f0e2f0e8222e2006158c0785d66da449a
rkBekzat <bekzatrakhimbayev@gmail.com>
2023-11-27 17:07:15 +0300

    lab12 done

--- app_python/index.py
+++ app_python/index.py
@@ -1,31 +1,36 @@
 from flask import Flask
 import pytz
+import os
 from datetime import datetime
 
 app = Flask(__name__)
 PATH = "./volume/visits.txt"
 
+def get_counter():
+    if not os.path.exists(PATH):
+        print(0, end='', file=open(PATH, 'w'))
+        return 0
+    else:
+        counts = 0
+        try:
+            counts = int(open(PATH, 'r').read().strip())
+        except Exception:
+            pass
+        return counts
 @app.route('/')
 def index():
     moscow_tz = pytz.timezone("Europe/Moscow")
     current_time = datetime.now(moscow_tz)
     try:
-        with open(PATH, "a+") as f:
-            f.write(str(current_time))
-            f.write('\n')
+        cnt = get_counter() + 1
+        print(cnt, end='', file=open(PATH, 'w'))
     except Exception as e:
         print(f"Errors when write the file: {e}")
     return f"{current_time.strftime('%Y:%m:%d %H:%M:%S')}"
 
 @app.route('/visits')
 def visits():
-    try:
-        with open(PATH, 'r') as f:
-            v = f.readlines()
-        return f"{len(v)}"
-    except Exception as e:
-        print(f"Error when read the file: {e}")
-    return "empty"
+    return f"{get_counter()}"
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080, host="0.0.0.0")
