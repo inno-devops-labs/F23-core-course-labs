685e021254834c5fde60ae0e57c3bb57af6c07c4
Murhaf Alawir <94635412+thecarrot123@users.noreply.github.com>
2023-11-28 19:11:14 +0300

    lab 12

--- app_python/main/app.py
+++ app_python/main/app.py
@@ -8,7 +8,19 @@ app = Flask(__name__)
 def moscow_time():
     moscow_timezone = pytz.timezone('Europe/Moscow')
     current_time_moscow = datetime.now(moscow_timezone)
+    with open("volume/visits.txt", 'a') as fl:
+        fl.write(current_time_moscow.strftime('%Y-%m-%d %H:%M:%S %Z'))
+        fl.write('\n')
+
     return f"Time in Moscow: {current_time_moscow.strftime('%Y-%m-%d %H:%M:%S %Z')}"
+
+
+@app.route('/visits/', methods=['GET'])
+def visits():
+    with open("volume/visits.txt", 'r') as fl:
+        visits = fl.readlines()
+    visits = '\n'.join(visits)
+    return visits
 
 if __name__ == '__main__':
     app.run(port=5050, debug=True)
