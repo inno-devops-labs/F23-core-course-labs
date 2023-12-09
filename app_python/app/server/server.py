c6ba3810c83db43691f9267380137895aa1e123e
beleet <105719971+beleet@users.noreply.github.com>
2023-12-09 21:34:08 +0300

    Add files via upload

---
+++ app_python/app/server/server.py
@@ -0,0 +1,18 @@
+from aiohttp import web
+from timeutils import timeutils
+
+
+async def time_handler(request):
+    return web.Response(text=timeutils.get_time())
+
+
+async def run_server(host: str, port: int):
+    app = web.Application()
+    app.add_routes(
+        [web.get('/time', time_handler)
+         ])
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, host=host, port=port)
+    await site.start()
+    print(f"Server started at http://{host}:{port}")
---
+++ app_python/app/timeutils/timeutils.py
@@ -0,0 +1,8 @@
+def get_time() -> str:
+    import datetime
+    from zoneinfo import ZoneInfo
+    dt = datetime.datetime.now(
+        tz=ZoneInfo("Europe/Moscow")
+    )
+    time = dt.strftime("%H:%M:%S")
+    return "Current time in Moscow: " + time
