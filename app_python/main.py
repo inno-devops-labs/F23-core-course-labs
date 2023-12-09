34eae7339066c3e13a4632492f846048dbc84b75
Vagif Khalilov <vakhalilov@ozon.ru>
2023-12-09 21:31:07 +0300

    update python

--- app_python/main.py
+++ app_python/main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from flask import Flask
 from datetime import datetime, timezone, timedelta
 from prometheus_flask_exporter import PrometheusMetrics
@@ -12,6 +13,22 @@ logging.basicConfig(
 
 logger = logging.getLogger("waitress")
 logger.setLevel(logging.INFO)
+path_to_volume = "./volume/visits.txt"
+
+def update_visit_count():
+    visit_count = 1
+    if os.path.exists(path_to_volume):
+        with open(path_to_volume, 'r') as file:
+            visit_count = int(file.read()) + 1
+    with open(path_to_volume, 'w') as file:
+        file.write(str(visit_count))
+
+
+def get_visit_count():
+    if not os.path.exists(path_to_volume):
+        return "0"
+    with open(path_to_volume, 'r') as file:
+        return file.read()
 
 
 @app.route('/')
@@ -19,7 +36,13 @@ def current_time():
     # Get time in Moscow
     moscow_time = datetime.now(timezone(timedelta(hours=3)))
     logger.info(msg=f'Method: GET Response: {moscow_time}')
+    update_visit_count()
     return f'Current time in Moscow: {moscow_time.strftime("%H:%M:%S")}'
+
+
+@app.route('/visits')
+def visits():
+    return f'Number of visits: {get_visit_count()}'
 
 
 if __name__ == '__main__':
