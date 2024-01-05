069bdef67068b3a34ee1d10b168b8b62d8d7d638
Acceptasis <acceptasis@Acceptasis.myguest.virtualbox.org>
2024-01-05 17:21:08 +0300

    lab3

--- app_python/test_app.py
+++ app_python/test_app.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 from app import app, display_time
 from datetime import datetime
 
+
 class FlaskAppTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -17,14 +18,14 @@ class FlaskAppTestCase(unittest.TestCase):
     @patch('app.datetime')
     def test_display_time_content(self, mock_datetime):
         # Set a fixed datetime for the test
-        fixed_datetime = datetime(2023, 1, 1, 12, 0, 0)
-        mock_datetime.now.return_value = fixed_datetime
+        fixtime = datetime(2023, 1, 1, 12, 0, 0)
+        mock_datetime.now.return_value = fixtime
 
         # Test if the content of the / route includes the expected time
         response = self.app.get('/')
-        expected_time = fixed_datetime.strftime('%Y-%m-%d %H:%M:%S')
-        self.assertIn(f'Current time in Moscow: {expected_time}', response.get_data(as_text=True))
+        expect = fixtime.strftime('%Y-%m-%d %H:%M:%S')
+        self.assertIn(f'Current time in Moscow: {expect}', response.get_data(as_text=True))
+
 
 if __name__ == '__main__':
     unittest.main()
-
