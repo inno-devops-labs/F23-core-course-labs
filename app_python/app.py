6caea49a4211186148c8d3931b728a08a09aaa87
Acceptasis <acceptasis@Acceptasis.myguest.virtualbox.org>
2024-01-12 23:16:17 +0300

    lab8

--- app_python/app.py
+++ app_python/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, Response
-from datetime import datetime
 from prometheus_client import generate_latest, Counter
 import pytz
 
@@ -7,15 +6,18 @@ app = Flask(__name__)
 
 healthcheck_counter = Counter('healthcheck_requests', 'Number of healthcheck requests')
 
+
 @app.route('/healthcheck')
 def healthcheck():
     healthcheck_counter.inc()  # Increment the healthcheck counter
     return 'Ok'
 
+
 # Define a route for metrics
 @app.route('/metrics')
 def metrics():
     return Response(generate_latest(), content_type='text/plain')
+
 
 # Define a route to display the current time in Moscow
 @app.route('/')
@@ -24,8 +26,6 @@ def display_time():
     moscow_time = datetime.now(moscow_timezone)
     formatted_time = moscow_time.strftime('%Y-%m-%d %H:%M:%S')
     return f'Current time in Moscow: {formatted_time}'
-
-
 
 
 if __name__ == '__main__':
