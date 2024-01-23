07cb6689d25d9555b788397027672a6362e7163c
Artemiy Kochergin <kocherginartem@mail.ru>
2024-01-23 20:47:32 +0300

    done lab 4

---
+++ app_node/app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    // Generate a random number between 1 and 21
+    const randomNumber = Math.floor(Math.random() * 21) + 1;
+
+    // Set the response headers
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    // Send the random number as the response
+    res.end(`Random Number: ${randomNumber}\n`);
+});
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
